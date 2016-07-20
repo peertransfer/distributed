60da96021569f478b9bd2d3c370a6f74a6434bd2
Jose Luis Salas <josacar@users.noreply.github.com>
2016-07-20 08:46:44 +0000

    Customize deploy

---
+++ DEPLOY.md
@@ -0,0 +1,9 @@
+## Build & deploy
+
+```bash
+sudo npm install -g firebase-tools
+rm -rf dist
+npm install
+npm run-script build
+npm run-script deploy
+```
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 LAST_COMMIT=$(git log -1 --pretty=format:'%h %an %s')
 echo "Deploying changes from $LAST_COMMIT"
-gh-pages -r git@github.com:funretro/distributed.git -d dist -m "Updates from $LAST_COMMIT"
+# gh-pages -r git@github.com:funretro/distributed.git -d dist -m "Updates from $LAST_COMMIT"
+cd dist
+npm_bin=$(npm bin)
+$npm_bin/firebase login
+$npm_bin/firebase deploy
